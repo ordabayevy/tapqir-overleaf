0f9d1b587ad04b8056b3c79367a2f4a695d7df11
Yerdos Ordabayev <ordabayev@brandeis.edu>
2021-12-11 18:12:28 -0500

    width

--- scripts/figures/tapqir_analysis.py
+++ scripts/figures/tapqir_analysis.py
@@ -9,7 +9,7 @@ from tapqir.models import Cosmos
 mpl.rc("text", usetex=True)
 mpl.rcParams.update({"font.size": 8})
 
-fig = plt.figure(figsize=(7.2, 5.4), constrained_layout=False)
+fig = plt.figure(figsize=(7.2, 6.4), constrained_layout=False)
 
 # panel a
 path_data = "simulations/lamda0.5"
@@ -21,13 +21,13 @@ frames = [100, 103, 105, 108, 110, 113, 115, 118, 120]
 f1, f2 = 100, 121
 vmin, vmax = model.data.vmin, model.data.vmax
 gs = fig.add_gridspec(
-    nrows=9,
+    nrows=11,
     ncols=1,
-    top=0.95,
-    bottom=0.08,
+    top=0.96,
+    bottom=0.07,
     left=0.12,
     right=0.54,
-    height_ratios=[1.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+    height_ratios=[1.5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 )
 
 gs1 = gs[0].subgridspec(2, 9)
@@ -122,9 +122,9 @@ ax.set_xlim(f1 - 0.5, f2 - 0.5)
 ax.set_ylim(-0.15, 1.15)
 ax.set_ylabel(r"$p(\mathsf{specific})$")
 
-# height
 for k in range(2):
-    ax = fig.add_subplot(gs[2 + k * 3, :])
+    # height
+    ax = fig.add_subplot(gs[2 + k * 4, :])
     mask = model.params["m_probs"][k, n, f1:f2] > 0.5
     ax.errorbar(
         x=torch.arange(f1, f2)[mask],
@@ -169,8 +169,54 @@ for k in range(2):
     ax.set_ylim(-500, 5500)
     ax.set_ylabel(r"$h$")
 
+    # width
+    ax = fig.add_subplot(gs[3 + k * 4, :])
+    mask = model.params["m_probs"][k, n, f1:f2] > 0.5
+    ax.errorbar(
+        x=torch.arange(f1, f2)[mask],
+        y=model.params["width"]["Mean"][k, n, f1:f2][mask],
+        yerr=torch.stack(
+            (
+                model.params["width"]["Mean"][k, n, f1:f2][mask]
+                - model.params["width"]["LL"][k, n, f1:f2][mask],
+                model.params["width"]["UL"][k, n, f1:f2][mask]
+                - model.params["width"]["Mean"][k, n, f1:f2][mask],
+            ),
+            0,
+        ),
+        fmt="o",
+        ms=2,
+        color=f"C{k}",
+        capsize=2,
+    )
+    plt.minorticks_on()
+    ax.tick_params(
+        direction="in",
+        which="minor",
+        length=1,
+        bottom=True,
+        top=True,
+        left=True,
+        right=True,
+    )
+    ax.tick_params(
+        direction="in",
+        which="major",
+        length=2,
+        bottom=True,
+        top=True,
+        left=True,
+        right=True,
+    )
+    ax.set_xticks(torch.arange(f1, f2 + 1, 5))
+    ax.set_yticks([1, 2])
+    ax.set_xticklabels([])
+    ax.set_xlim(f1 - 0.5, f2 - 0.5)
+    ax.set_ylim(0.5, 2.5)
+    ax.set_ylabel(r"$w$")
+
     # x
-    ax = fig.add_subplot(gs[3 + k * 3, :])
+    ax = fig.add_subplot(gs[4 + k * 4, :])
     mask = model.params["m_probs"][k, n, f1:f2] > 0.5
     ax.errorbar(
         x=torch.arange(f1, f2)[mask],
@@ -217,7 +263,7 @@ for k in range(2):
     if k == 0:
         ax.text(
             f1 - 5,
-            0,
+            12,
             "spot 1",
             color="C0",
             rotation=90,
@@ -226,7 +272,7 @@ for k in range(2):
     elif k == 1:
         ax.text(
             f1 - 5,
-            0,
+            12,
             "spot 2",
             color="C1",
             rotation=90,
@@ -236,7 +282,7 @@ for k in range(2):
         f1 - 4,
         -30,
         0,
-        60,
+        82,
         length_includes_head=True,
         head_width=0.0,
         head_length=0.0,
@@ -245,7 +291,7 @@ for k in range(2):
     )
     ax.arrow(
         f1 - 4,
-        30,
+        52,
         0.5,
         0,
         length_includes_head=True,
@@ -267,7 +313,7 @@ for k in range(2):
     )
 
     # y
-    ax = fig.add_subplot(gs[4 + k * 3, :])
+    ax = fig.add_subplot(gs[5 + k * 4, :])
     mask = model.params["m_probs"][k, n, f1:f2] > 0.5
     ax.errorbar(
         x=torch.arange(f1, f2)[mask],
@@ -313,7 +359,7 @@ for k in range(2):
     ax.set_ylabel(r"$y$")
 
 # background
-ax = fig.add_subplot(gs[8, :])
+ax = fig.add_subplot(gs[10, :])
 ax.errorbar(
     x=torch.arange(f1, f2),
     y=model.params["background"]["Mean"][n, f1:f2],
@@ -366,10 +412,10 @@ for i, f in enumerate(frames):
             [
                 (f - 0.2, 0),
                 (f + 0.2, 0),
-                (f + 0.2, 9.48 * 500),
-                (f1 - 0.5 + 0.5 * x + (h + x) * i + x / 2, 9.66 * 500),
-                (f1 - 0.5 + 0.5 * x + (h + x) * i - x / 2, 9.66 * 500),
-                (f - 0.2, 9.48 * 500),
+                (f + 0.2, 11.89 * 500),
+                (f1 - 0.5 + 0.5 * x + (h + x) * i + x / 2, 12.07 * 500),
+                (f1 - 0.5 + 0.5 * x + (h + x) * i - x / 2, 12.07 * 500),
+                (f - 0.2, 11.89 * 500),
             ],
             clip_on=False,
             zorder=100,
@@ -389,14 +435,13 @@ frames = [625, 628, 630, 633, 635, 638, 640, 643, 645]
 vmin, vmax = 340, 635
 
 gs = fig.add_gridspec(
-    nrows=9,
+    nrows=11,
     ncols=1,
-    top=0.95,
-    bottom=0.08,
+    top=0.96,
+    bottom=0.07,
     left=0.57,
     right=0.99,
-    width_ratios=[9],
-    height_ratios=[1.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+    height_ratios=[1.5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 )
 gs1 = gs[0].subgridspec(2, 9)
 
@@ -443,7 +488,7 @@ for i, f in enumerate(frames):
 ax = fig.add_subplot(gs[1, :])
 ax.plot(
     torch.arange(f1, f2),
-    model.params["p(specific)"][n, f1:f2],
+    model.params["z_probs"][n, f1:f2],
     "o-",
     ms=2,
     lw=0.5,
@@ -475,9 +520,9 @@ ax.set_yticklabels([])
 ax.set_xlim(f1 - 0.5, f2 - 0.5)
 ax.set_ylim(-0.15, 1.15)
 
-# height
 for k in range(2):
-    ax = fig.add_subplot(gs[2 + k * 3, :])
+    # height
+    ax = fig.add_subplot(gs[2 + k * 4, :])
     mask = model.params["m_probs"][k, n, f1:f2] > 0.5
     ax.errorbar(
         x=torch.arange(f1, f2)[mask],
@@ -522,8 +567,54 @@ for k in range(2):
     ax.set_xlim(f1 - 0.5, f2 - 0.5)
     ax.set_ylim(-500, 5500)
 
+    # width
+    ax = fig.add_subplot(gs[3 + k * 4, :])
+    mask = model.params["m_probs"][k, n, f1:f2] > 0.5
+    ax.errorbar(
+        x=torch.arange(f1, f2)[mask],
+        y=model.params["width"]["Mean"][k, n, f1:f2][mask],
+        yerr=torch.stack(
+            (
+                model.params["width"]["Mean"][k, n, f1:f2][mask]
+                - model.params["width"]["LL"][k, n, f1:f2][mask],
+                model.params["width"]["UL"][k, n, f1:f2][mask]
+                - model.params["width"]["Mean"][k, n, f1:f2][mask],
+            ),
+            0,
+        ),
+        fmt="o",
+        ms=2,
+        color=f"C{k}",
+        capsize=2,
+    )
+    plt.minorticks_on()
+    ax.tick_params(
+        direction="in",
+        which="minor",
+        length=1,
+        bottom=True,
+        top=True,
+        left=True,
+        right=True,
+    )
+    ax.tick_params(
+        direction="in",
+        which="major",
+        length=2,
+        bottom=True,
+        top=True,
+        left=True,
+        right=True,
+    )
+    ax.set_xticks(torch.arange(f1, f2 + 1, 5))
+    ax.set_yticks([1, 2])
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    ax.set_xlim(f1 - 0.5, f2 - 0.5)
+    ax.set_ylim(0.5, 2.5)
+
     # x
-    ax = fig.add_subplot(gs[3 + k * 3, :])
+    ax = fig.add_subplot(gs[4 + k * 4, :])
     mask = model.params["m_probs"][k, n, f1:f2] > 0.5
     ax.errorbar(
         x=torch.arange(f1, f2)[mask],
@@ -569,7 +660,7 @@ for k in range(2):
     ax.set_ylim(-9, 9)
 
     # y
-    ax = fig.add_subplot(gs[4 + k * 3, :])
+    ax = fig.add_subplot(gs[5 + k * 4, :])
     mask = model.params["m_probs"][k, n, f1:f2] > 0.5
     ax.errorbar(
         x=torch.arange(f1, f2)[mask],
@@ -615,7 +706,7 @@ for k in range(2):
     ax.set_ylim(-9, 9)
 
 # background
-ax = fig.add_subplot(gs[8, :])
+ax = fig.add_subplot(gs[10, :])
 ax.errorbar(
     x=torch.arange(f1, f2),
     y=model.params["background"]["Mean"][n, f1:f2],
@@ -668,10 +759,10 @@ for i, f in enumerate(frames):
             [
                 (f - 0.2, 0),
                 (f + 0.2, 0),
-                (f + 0.2, 9.48 * 500),
-                (f1 - 0.5 + 0.5 * x + (h + x) * i + x / 2, 9.66 * 500),
-                (f1 - 0.5 + 0.5 * x + (h + x) * i - x / 2, 9.66 * 500),
-                (f - 0.2, 9.48 * 500),
+                (f + 0.2, 11.89 * 500),
+                (f1 - 0.5 + 0.5 * x + (h + x) * i + x / 2, 12.07 * 500),
+                (f1 - 0.5 + 0.5 * x + (h + x) * i - x / 2, 12.07 * 500),
+                (f - 0.2, 11.89 * 500),
             ],
             clip_on=False,
             zorder=100,
